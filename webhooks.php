eb02965973aab4d2cdc648ee78a2dc137e8cdc69
Khem <ittipatk@gmail.com>
2018-09-05 10:54:02 +0700

    return same text

--- webhooks.php
+++ webhooks.php
@@ -15,8 +15,9 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			
 			// Get text sent
-			$text = $event['source']['userId'];
+			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
