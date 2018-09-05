ebf493cf95846114ca7ebbec3cd5df9ade79e940
Khem <ittipatk@gmail.com>
2018-09-05 10:57:06 +0700

    get emp_code

--- webhooks.php
+++ webhooks.php
@@ -16,8 +16,14 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			
+			
 			// Get text sent
 			$text = $event['message']['text'];
+
+			$emp_code = substr($text,0,6);
+
+			$text = $emp_code;
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
