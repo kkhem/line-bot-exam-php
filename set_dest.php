7a553214d14953e358cf9a306da07b0966ad1096
Khem <ittipatk@gmail.com>
2019-03-07 10:51:51 +0700

    test emp_code

--- set_dest.php
+++ set_dest.php
@@ -1,29 +1,35 @@
 <?php
 
-$request = new HttpRequest();
-$request->setUrl('https://webexternal.nok.co.th/boardlinebot/api/Dest');
-$request->setMethod(HTTP_METH_POST);
+ 
+$emp_code = $_GET['email'];
 
-$request->setQueryData(array(
-  'emp_code' => '160039',
-  'location' => 'pln',
-  'remark' => ''
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+CURLOPT_URL => "https://webexternal.nok.co.th/boardlinebot/api/Dest?emp_code={$emp_code}&location=osp1&remark=dsdsd",
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => "",
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 30,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => "POST",
+CURLOPT_POSTFIELDS => "emp_code=160039&location=osp1&remark=&undefined=",
+CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/x-www-form-urlencoded",
+    "Postman-Token: 6a58649a-49b1-4e23-9010-e920bd1f387f",
+    "cache-control: no-cache"
+),
 ));
 
-$request->setHeaders(array(
-  'cache-control' => 'no-cache',
-  'Content-Type' => 'application/x-www-form-urlencoded'
-));
+$response = curl_exec($curl);
+$err = curl_error($curl);
 
-$request->setContentType('application/x-www-form-urlencoded');
-$request->setPostFields(array(
-  'undefined' => null
-));
+curl_close($curl);
 
-try {
-  $response = $request->send();
-
-  echo $response->getBody();
-} catch (HttpException $ex) {
-  echo $ex;
+if ($err) {
+echo "cURL Error #:" . $err;
+} else {
+echo $response;
 }
+
+?>
