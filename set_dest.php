44f4168b7be37058a96b2fc7dd85d548920612dc
Khem <ittipatk@gmail.com>
2019-03-07 10:25:19 +0700

    change file set dest

---
+++ set_dest.php
@@ -0,0 +1,32 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+CURLOPT_URL => "http://webexternal/boardlinebot/api/Dest?emp_code=160039&location=osp1&remark=dsdsd",
+CURLOPT_RETURNTRANSFER => true,
+CURLOPT_ENCODING => "",
+CURLOPT_MAXREDIRS => 10,
+CURLOPT_TIMEOUT => 30,
+CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+CURLOPT_CUSTOMREQUEST => "POST",
+CURLOPT_POSTFIELDS => "emp_code=160039&location=osp1&remark=&undefined=",
+CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/x-www-form-urlencoded",
+    "Postman-Token: 6a58649a-49b1-4e23-9010-e920bd1f387f",
+    "cache-control: no-cache"
+),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+echo "cURL Error #:" . $err;
+} else {
+echo $response;
+}
+
+?>
